d79cfa564a72503669e2f641d7771c7d6583b21c
Felipe Corrêa <fcsantos.ap@gmail.com>
2017-01-11 20:27:46 -0200

    first commit

---
+++ ChatSignalR/ChatSignalR/Messages/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR; //SignalR
+
+namespace ChatSignalR.Messages
+{
+    //HUB -> conversação
+    public class ChatHub : Hub
+    {
+        //método para gerar uma conversa
+        //Send -> método para enviar uma mensagem
+        public void Send(string usuario, string mensagem)
+        {
+            //transmitir a conversa para os ouvintes
+            //quem poderá ouvir a conversa é qualquer um que 
+            //tenha acesso a este hub
+            Clients.All.broadcastMessage(usuario, mensagem);
+        }
+    }
+}
---
+++ ChatSignalR/ChatSignalR/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Owin;
+using Microsoft.Owin;
+
+//mapeamento para ativar as configurações do OWIN
+[assembly:OwinStartup(typeof(ChatSignalR.Startup))]
+namespace ChatSignalR
+{
+    public class Startup
+    {
+        //método de configuração as tecnologias
+        public void Configuration(IAppBuilder app)
+        {
+            //habilitar o signalR
+            app.MapSignalR();
+        }
+    }
+}
